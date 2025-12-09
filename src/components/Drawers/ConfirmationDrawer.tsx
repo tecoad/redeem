4ccc5f99ef670d254796eb3d8f66c1f124a4b3df
Matheus <tecoad@gmail.com>
2025-12-09 09:25:26 -0300

    ci: fix build

--- src/components/Drawers/ConfirmationDrawer.tsx
+++ src/components/Drawers/ConfirmationDrawer.tsx
@@ -1,10 +1,5 @@
 import { useRouter } from "@tanstack/react-router"
-import {
-	IconBanFillDuo18,
-	IconCircleDollarOutFillDuo18,
-	IconMenuFillDuo18,
-	IconShieldCheckFillDuo18,
-} from "nucleo-ui-fill-duo-18"
+import { IconCircleDollarOutFillDuo18 } from "nucleo-ui-fill-duo-18"
 import { ConfirmButton } from "../ConfirmButton"
 import Drawer, { type DrawerProps } from "../Drawer"
 import KeyValueList from "../KeyValueList"
@@ -43,23 +38,23 @@ function ConfirmationDrawer(props: DrawerProps) {
 	)
 }
 
-function _List() {
-	return (
-		<ul className="mt-6 space-y-4 border-t border-[#F5F5F5] pt-6">
-			<li className="flex items-center gap-3 text-[15px] font-semibold text-[#999999] md:font-medium">
-				<IconShieldCheckFillDuo18 className="size-6" />
-				Keep your Secret Phrase safe
-			</li>
-			<li className="flex items-center gap-3 text-[15px] font-semibold text-[#999999] md:font-medium">
-				<IconMenuFillDuo18 className="size-6" />
-				Don’t share it with anyone else
-			</li>
-			<li className="flex items-center gap-3 text-[15px] font-semibold text-[#999999] md:font-medium">
-				<IconBanFillDuo18 className="size-6" />
-				If you lose it, we can’t recover it
-			</li>
-		</ul>
-	)
-}
+// function _List() {
+// 	return (
+// 		<ul className="mt-6 space-y-4 border-t border-[#F5F5F5] pt-6">
+// 			<li className="flex items-center gap-3 text-[15px] font-semibold text-[#999999] md:font-medium">
+// 				<IconShieldCheckFillDuo18 className="size-6" />
+// 				Keep your Secret Phrase safe
+// 			</li>
+// 			<li className="flex items-center gap-3 text-[15px] font-semibold text-[#999999] md:font-medium">
+// 				<IconMenuFillDuo18 className="size-6" />
+// 				Don’t share it with anyone else
+// 			</li>
+// 			<li className="flex items-center gap-3 text-[15px] font-semibold text-[#999999] md:font-medium">
+// 				<IconBanFillDuo18 className="size-6" />
+// 				If you lose it, we can’t recover it
+// 			</li>
+// 		</ul>
+// 	)
+// }
 
 export default ConfirmationDrawer
